8a6eaf440d2baf5571256e6646b89aec34498d78
panda1835 <lehoangphuc1820@gmail.com>
2025-12-05 17:55:50 +0700

    edit opengraph image

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -16,57 +16,20 @@ export default async function Image() {
     (
       <div
         style={{
-          fontSize: 64,
           background: "linear-gradient(to bottom, #1a472a, #2d5a3d)",
           width: "100%",
           height: "100%",
           display: "flex",
-          flexDirection: "column",
           alignItems: "center",
           justifyContent: "center",
-          color: "white",
-          fontFamily: "sans-serif",
         }}
       >
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            gap: "20px",
-          }}
-        >
-          <h1
-            style={{
-              fontSize: 72,
-              fontWeight: "bold",
-              margin: 0,
-              textAlign: "center",
-            }}
-          >
-            A Thousand Wishes
-          </h1>
-          <p
-            style={{
-              fontSize: 36,
-              margin: 0,
-              textAlign: "center",
-              opacity: 0.9,
-            }}
-          >
-            Bringing Joy to Children in Vietnam and Mongolia
-          </p>
-          <p
-            style={{
-              fontSize: 28,
-              margin: 0,
-              marginTop: "40px",
-              opacity: 0.8,
-            }}
-          >
-            Christina Noble Children&apos;s Foundation Christmas Campaign 2025
-          </p>
-        </div>
+        <img
+          src="/CNCF-noel-logo.png"
+          alt="CNCF Noel Logo"
+          width="400"
+          height="400"
+        />
       </div>
     ),
     {
